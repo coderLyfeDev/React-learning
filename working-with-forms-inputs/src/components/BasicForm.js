1d9828ab5ad972096d48392ad1a5b5d3d761816a
brandonharshaw <brandonharshaw24@gmail.com>
2022-09-16 08:10:19 -0700

    Finished form validation module

--- working-with-forms-inputs/src/components/BasicForm.js
+++ working-with-forms-inputs/src/components/BasicForm.js
@@ -1,22 +1,76 @@
+import useInput from '../hooks/use-input';
+
 const BasicForm = (props) => {
+  let formIsValid = false;
+  const {
+    value:entereedFirstNameValue, 
+    isValid: valueFirstNameIsValid,
+    hasError: firstNameHasError,
+    valueInputChangeHandler: firstNameValueInputChaneHandler, 
+    InputBlurHandler: firstNameBlurHandler,
+    reset: firstNameReset
+    } = useInput(value => value.trim() !== '');
+  
+    const {
+      value:entereedLastNameValue, 
+      isValid: valueLastNameIsValid,
+      hasError: lastNameHasError,
+      valueInputChangeHandler: lastNameValueInputChaneHandler, 
+      InputBlurHandler: lastNameBlurHandler,
+      reset: lastNameReset
+      } = useInput(value => value.trim() !== '');
+  
+      const {
+        value:entereedEmailValue, 
+        isValid: valueEmailIsValid,
+        hasError: emailHasError,
+        valueInputChangeHandler: emailValueInputChaneHandler, 
+        InputBlurHandler: emailBlurHandler,
+        reset: emailReset
+        } = useInput(value => value.includes("@"));
+
+
+        formIsValid = valueEmailIsValid && valueFirstNameIsValid && valueLastNameIsValid;
+
+        const firstNameClass = firstNameHasError? 'form-control invalid': 'form-control';
+        const lastNameClass = lastNameHasError? 'form-control invalid': 'form-control';
+        const emailClass = emailHasError? 'form-control invalid': 'form-control';
+  
+        const formValidation = event => {
+          event.preventDefault();
+          if(valueFirstNameIsValid && valueLastNameIsValid && valueEmailIsValid){
+            console.log(entereedFirstNameValue);
+            console.log(entereedLastNameValue);
+            console.log(entereedEmailValue);
+            firstNameReset();
+            lastNameReset();
+            emailReset();
+          }
+          return;
+        };
+
   return (
-    <form>
+    <form onSubmit={formValidation}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClass}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={firstNameValueInputChaneHandler} onBlur={firstNameBlurHandler} value={entereedFirstNameValue}/>
+          {firstNameHasError && <p className='error-text'>First name Cannot be empty</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClass}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={lastNameValueInputChaneHandler} onBlur={lastNameBlurHandler} value = {entereedLastNameValue}/>
+          {lastNameHasError && <p className='error-text'>First name Cannot be empty</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailClass}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input type='email' id='name' onChange={emailValueInputChaneHandler} onBlur={emailBlurHandler} value = {entereedEmailValue}/>
+        {emailHasError && <p className='error-text'>Email invalid</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
+        
       </div>
     </form>
   );
